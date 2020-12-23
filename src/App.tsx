492d463adfa8137ebc3234a5e76efe0c979f8e05
mert <uzeyirmertciflikli@gmail.com>
2020-12-23 16:09:50 +0100

    add Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import {AppContainer} from './styles'
+import {Column} from './Column'
+import {Card} from './Card'
 
 function App() {
   return (
     <AppContainer >
-Columns will go here
+      <Column text="To Do">
+        <Card text="Generate app scaffold"></Card>
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript"></Card>
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing"></Card>
+      </Column>
     </AppContainer>
   );
 }
