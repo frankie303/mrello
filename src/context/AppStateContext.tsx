0e6f98d37aaaf034395ad2693d0f42722214be6d
mert <uzeyirmertciflikli@gmail.com>
2021-01-03 15:43:46 +0100

    tweak

--- src/context/AppStateContext.tsx
+++ src/context/AppStateContext.tsx
@@ -24,17 +24,17 @@ const appData: AppState = {
     {
       id: '0',
       text: 'To Do',
-      tasks: [{ id: 'c0', text: 'Generate app scaffold' }]
+      tasks: [{ id: 'c0', text: 'Lorem, ipsum dolor.' }]
     },
     {
       id: '1',
       text: 'In Progress',
-      tasks: [{ id: 'c2', text: 'Learn Typescript' }]
+      tasks: [{ id: 'c2', text: 'Lorem, ipsum.' }]
     },
     {
       id: '2',
       text: 'Done',
-      tasks: [{ id: 'c3', text: 'Begin to use static typing' }]
+      tasks: [{ id: 'c3', text: 'Lorem ipsum dolor sit.' }]
     }
   ]
 };
